d77663abda8492473253701ee75cab8dc94589d4
Alcides Rivera <elarsi626@gmail.com>
2026-03-12 15:31:49 +0000

    Calculo distancia y precio

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Address, UserRole, Store
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, calculate_distance, calculate_order_price
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from sqlalchemy import select
